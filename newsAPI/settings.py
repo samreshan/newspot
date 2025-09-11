a3950ae65af319e643a3a9a9f6478d04456156c4
Samreshan Sahani <138707074+samreshan@users.noreply.github.com>
2025-09-11 22:49:26 +0545

    Update settings.py
    allowed host

--- newsAPI/settings.py
+++ newsAPI/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -25,7 +26,7 @@ SECRET_KEY = 'django-insecure-k1c=7y)f2lo2-04@du(++w(+3d^-ep+53nwhc@ef=mpf9#ob6c
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = os.environ.get("DJANGO_ALLOWED_HOSTS", "localhost 127.0.0.1").split(" ")
 
 
 # Application definition
